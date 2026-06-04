94efe34e9574fe056224440c85c8082bec121cb1
Halkerov <155100720+Halkerov@users.noreply.github.com>
2026-06-04 21:16:31 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-	path('register/', views.register, name='register'),
-	path('login/', views.login_view, name='login'),
-	path('logout/', views.logout_view, name='logout'),
-	path('create/', views.create_post, name='create_post'),
+    path('', views.index, name='index'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
+    path('post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('create/', views.create_post, name='create_post'),
 ]
